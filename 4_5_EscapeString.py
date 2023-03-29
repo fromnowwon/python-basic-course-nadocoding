7f87dc5231edc068175c3d50d584ee2912995330
cw <fromnowwon@gmail.com>
2023-03-29 21:02:28 +0900

    feat: 4_5_EscapeString

---
+++ 4_5_EscapeString.py
@@ -0,0 +1,21 @@
+# 탈출 문자
+
+# 줄바꿈
+print("백문이 불여일견 \n백견이 불여일타")
+
+# 문장 내에서 따옴표 사용
+print("저는 '나도코딩'입니다.")
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.")
+
+# 문장 내에서 역슬래시
+print("C:\\Users\\Nadocoding\\Desktop")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스
+print("Redd\b Apple")
+
+# \t : 탭
+print("Red\tApple")
