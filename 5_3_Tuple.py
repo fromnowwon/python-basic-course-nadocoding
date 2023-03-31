8b21e4d5fc77497bc04995ab32d8dc5a139fd663
cw <fromnowwon@gmail.com>
2023-03-31 21:07:42 +0900

    feat: 5_3_Tuple

---
+++ 5_3_Tuple.py
@@ -0,0 +1,16 @@
+# 튜플
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스") 에러
+
+# 변수 방식
+name = "김종국"
+age = 20
+hobby = "코딩"
+
+# 튜플 방식
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
